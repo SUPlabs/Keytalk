bc8054bb78e82df0df3c4ab3a0640b98d9d08826
Ronald Groennou <r.groennou@superp.nl>
2016-06-16 08:50:02 +0200

    1

--- www/keytalk.js
+++ www/keytalk.js
@@ -19,54 +19,9 @@
  *
  */
 
-var argscheck = require('cordova/argscheck'),
-channel = require('cordova/channel'),
-utils = require('cordova/utils'),
-exec = require('cordova/exec'),
-cordova = require('cordova');
 
-channel.createSticky('onCordovaInfoReady');
-// Tell cordova channel to wait on the CordovaInfoReady event
-channel.waitForInitialization('onCordovaInfoReady');
-
-/**
- * This represents the mobile device, and provides properties for inspecting the model, version, UUID of the
- * phone, etc.
- * @constructor
- */
 function Keytalk() {
-    this.available = false;
-    this.platform = null;
-    this.version = null;
-    this.uuid = null;
-    this.cordova = null;
-    this.model = null;
-    this.manufacturer = null;
-    this.isVirtual = null;
-    this.serial = null;
-    
-    var me = this;
-    
-    channel.onCordovaReady.subscribe(function() {
-                                     me.getInfo(function(info) {
-                                                //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
-                                                //TODO: CB-5105 native implementations should not return info.cordova
-                                                var buildLabel = cordova.version;
-                                                me.available = true;
-                                                me.platform = info.platform;
-                                                me.version = info.version;
-                                                me.uuid = info.uuid;
-                                                me.cordova = buildLabel;
-                                                me.model = info.model;
-                                                me.isVirtual = info.isVirtual;
-                                                me.manufacturer = info.manufacturer || 'unknown';
-                                                me.serial = info.serial || 'unknown';
-                                                channel.onCordovaInfoReady.fire();
-                                                },function(e) {
-                                                me.available = false;
-                                                utils.alert("[ERROR] Error initializing Cordova: " + e);
-                                                });
-                                     });
+
 }
 
 /**
@@ -76,8 +31,8 @@ function Keytalk() {
  * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
  */
 Keytalk.prototype.getInfo = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'Device.getInfo', arguments);
-    exec(successCallback, errorCallback, "Device", "getDeviceInfo", []);
+    // argscheck.checkArgs('fF', 'Device.getInfo', arguments);
+    // exec(successCallback, errorCallback, "Device", "getDeviceInfo", []);
 };
 
 module.exports = new Keytalk();
